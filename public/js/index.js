a59bdf118a6e01995458bcc1b8c527a0e12fa9dd
Michael Campos <camposmj89@gmail.com>
2017-07-11 22:32:07 -0700

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,13 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function(e) {
 	e.preventDefault();
 
+	var messageTextbox = $('[name=message]');
+
 	socket.emit('createMessage', {
 		from: 'User',
-		text: $('[name=message]').val()
-	}, function(data) {
-
+		text: messageTextbox.val()
+	}, function() {
+		messageTextbox.val('');
 	});
 });
 
@@ -46,12 +48,17 @@ locationButton.on('click', function() {
 		return alert('Geolocation not supported by your browswer');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(function(position) {
+		locationButton.removeAttr('disabled').text('Send location');
+		
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, function() {
+		locationButton.removeAttr('disabled').text('Send location');
 		alert('Unable to fetch location.');
 	});
 });
